f38e4baa837104424871563d8ff34be3d30f3cd9
bhabesh2 <jsarma058@gmail.com>
2023-05-18 21:45:13 +0530

    done Array problems

--- DSA classes/arrays/programe13.cpp
+++ DSA classes/arrays/programe13.cpp
@@ -1,9 +1,47 @@
-
-#include <bits/stdc++.h>;
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n;
-    cin>>n;
-    
-}
+// minimum and maximum number finding;
+
+// int main() {
+//     int n;
+//     cin >> n;
+
+//     int arr[n];// input size of an array;
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];// input taking according to its predefined size;
+//     }
+
+//     int minNo = INT_MAX;
+//     int maxNo = INT_MIN;
+
+//     for (int i = 0; i < n; i++) {
+//         minNo = min(minNo, arr[i]);
+//         maxNo = max(maxNo, arr[i]);
+//     }
+
+//     cout <<"minNo is-->"<< minNo << " " << "maxNo-->"<< maxNo << endl;
+
+//     return 0;
+// }
+
+// running sum of an array.
+
+// int main(){
+//     int n;
+//     cin>>n;
+
+//     int arr[n];
+//     for(int i = 0; i < n; i++){
+//         cin>>arr[i];
+//     }
+
+//     int runningSum = 0;
+//     for(int i = 0; i < n; i++){
+//         runningSum += arr[i];
+//         cout<<runningSum<<endl;
+//     }
+//     return 0;
+// }
+
+// kids with greatest number od candies done; in leetcode;
